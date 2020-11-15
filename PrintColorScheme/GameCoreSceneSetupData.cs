7657c075ce2d1c889eaa6cbb82675c01842d96b8
Aeroluna <aeroluna5@gmail.com>
2020-11-15 16:46:08 -0700

    Add project files.

---
+++ PrintColorScheme/GameCoreSceneSetupData.cs
@@ -0,0 +1,35 @@
+﻿namespace PrintColorScheme
+{
+    using HarmonyLib;
+    using UnityEngine;
+    using IPALogger = IPA.Logging.Logger;
+
+    [HarmonyPatch(typeof(StandardLevelScenesTransitionSetupDataSO))]
+    [HarmonyPatch("Init")]
+    internal static class GameCoreSceneSetupDataCtor
+    {
+        private static void Postfix(IDifficultyBeatmap difficultyBeatmap, ColorScheme overrideColorScheme)
+        {
+            IPALogger logger = Plugin.Logger;
+            EnvironmentInfoSO environmentInfoSO = difficultyBeatmap.GetEnvironmentInfo();
+            ColorScheme colorScheme = overrideColorScheme ?? new ColorScheme(environmentInfoSO.colorScheme);
+            logger.Info("===================");
+            logger.Info("ACTIVE COLOR SCHEME:");
+            logger.Info($"colorSchemeId: {colorScheme.colorSchemeId}");
+            logger.Info($"colorSchemeName: {colorScheme.colorSchemeName}");
+            logger.Info($"supportsEnvironmentColorBoost: {colorScheme.supportsEnvironmentColorBoost}");
+            logger.Info($"saberAColor: {PrintFullColor(colorScheme.saberAColor)}");
+            logger.Info($"saberBColor: {PrintFullColor(colorScheme.saberBColor)}");
+            logger.Info($"environmentColor0: {PrintFullColor(colorScheme.environmentColor0)}");
+            logger.Info($"environmentColor1: {PrintFullColor(colorScheme.environmentColor1)}");
+            logger.Info($"environmentColor0Boost: {PrintFullColor(colorScheme.environmentColor0Boost)}");
+            logger.Info($"environmentColor1Boost: {PrintFullColor(colorScheme.environmentColor1Boost)}");
+            logger.Info($"obstaclesColor: {PrintFullColor(colorScheme.obstaclesColor)}");
+            logger.Info("===================");
+        }
+        private static string PrintFullColor(Color color)
+        {
+            return $"{{ {color.r}, {color.g}, {color.b} }}";
+        }
+    }
+}
---
+++ PrintColorScheme/Plugin.cs
@@ -0,0 +1,35 @@
+﻿namespace PrintColorScheme
+{
+    using System.Reflection;
+    using HarmonyLib;
+    using IPA;
+    using IPALogger = IPA.Logging.Logger;
+
+    [Plugin(RuntimeOptions.DynamicInit)]
+    internal class Plugin
+    {
+        internal static IPALogger Logger { get; private set; }
+
+        private const string HARMONYID = "com.noodle.BeatSaber.PrintColorScheme";
+
+        private static readonly Harmony _harmonyInstance = new Harmony(HARMONYID);
+
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Logger = logger;
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            _harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            _harmonyInstance.UnpatchAll(HARMONYID);
+        }
+    }
+}
